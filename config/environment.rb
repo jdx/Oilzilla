ee846635c0dfee886cd453a854bcc23319941566
dickeytk <dickeytk@gmail.com>
2010-05-01 19:25:07 -0700

    removed gem reference

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,6 @@ RAILS_GEM_VERSION = '2.2.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "authlogic"
   config.time_zone = 'UTC'
   config.action_controller.session = {
     :session_key => '_conceptor_session',
