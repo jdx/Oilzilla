52a48b257282f2ef0645fcadecc1471443fe5800
dickeytk <dickeytk@gmail.com>
2010-05-01 19:34:21 -0700

    maybe fixed it

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,13 +1,24 @@
 begin
+  # Set up load paths for the locked set of pre-resolved gems.
+  require File.expand_path('../../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on trying to resolve out of already-installed gems at runtime.
   require "rubygems"
   require "bundler"
 
-  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.20")
-    raise RuntimeError, "Your bundler version is too old." +
-     "Run `gem install bundler` to upgrade."
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
+    raise RuntimeError,
+      "Your bundler version is incompatible with Rails 2.3 and an unlocked bundle.\n" +
+      "Run `gem install bundler` to upgrade or `bundle lock` to lock."
   end
 
-  # Set up load paths for all bundled gems
-  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
-  Bundler.setup
+  begin
+    # Set up load paths for all bundled gems
+    Bundler.setup
+    # Require all gems not in a group
+    Bundler.require :default
+  rescue Bundler::GemNotFound
+    raise Bundler::GemNotFound, "Bundler couldn't find some gems. Did you run `bundle install`?"
+  end
 end
+
